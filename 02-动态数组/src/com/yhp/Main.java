c2588974cca0264b1550062ff9599a1896780646
ALanHua <yhp-2009@qq.com>
2021-02-06 15:59:56 +0800

    双向链表

--- 02-动态数组/src/com/yhp/Main.java
+++ 02-动态数组/src/com/yhp/Main.java
@@ -2,13 +2,27 @@ package com.yhp;
 
 public class Main {
 
-	public static void main(String[] args) {
+	static void testArrayList() {
 		ArrayList<Integer> ints  = new ArrayList<>();
 		for (int i = 0; i < 20; i++) {
 			ints.add(i);
 		}
 		ints.remove(2);
 		System.out.println(ints);
+	}
+	
+	static void testLinkedList() {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(11);
+		list.add(22);
+		list.add(33);
+		list.add(44);
+		list.add(list.size,55);
+		System.out.println(list);
+	}
+	
+	public static void main(String[] args) {
+		testLinkedList();
 	}
 
 }
